5de74ca3e83813dff26fde33d8f88ada7d6ed1bb
mtorres9095 <mtorres9095@gmail.com>
2020-06-08 16:00:51 -0400

    Commented out the ToString and Equals

--- SchoolPractice/Course.cs
+++ SchoolPractice/Course.cs
@@ -13,7 +13,7 @@ namespace SchoolPractice
         {
 
         }
-        public override bool Equals(object toBeCompared)//5.3.2.1.1. Problem #1 
+        /*public override bool Equals(object toBeCompared)//5.3.2.1.1. Problem #1 
         {
             if (toBeCompared.GetType() != this.GetType())
             {
@@ -23,12 +23,12 @@ namespace SchoolPractice
             Student s = toBeCompared as Student;
             return s.StudentId == StudentId;
         }
-        //Student bono1 = new Student("Paul David Hewson", 4);
-        //Student bono2 = new Student("Bono", 4);
-        //if (bono1.Equals(bono2))
-           // {
-           // Console.WriteLine(bono1.Name + " is the same as " + bono2.Name);
-           // }
+        Student bono1 = new Student("Paul David Hewson", 4);
+        Student bono2 = new Student("Bono", 4);
+        if (bono1.Equals(bono2))
+           {
+            Console.WriteLine(bono1.Name + " is the same as " + bono2.Name);
+            }*/
 }
 }
 
